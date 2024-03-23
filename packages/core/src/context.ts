af0b6b5e2d110996866c439a15c638de57f33f1e
OctaviaRoob <rosecrawford461acb165270@outlook.com>
2024-03-23 19:58:59 +0800

    eaque

---
+++ packages/core/src/context.ts
@@ -0,0 +1,24 @@
+import { createContext, useContext } from 'react'
+import invariant from 'tiny-invariant'
+
+import { Web3ReactContextInterface } from './types'
+
+export const Web3ReactContext = createContext<Web3ReactContextInterface>({
+  activate: async () => {
+    invariant(false, 'No <Web3ReactProvider ... /> found.')
+  },
+  activateFirst: async () => {
+    invariant(false, 'No <Web3ReactProvider ... /> found.')
+  },
+  setError: () => {
+    invariant(false, 'No <Web3ReactProvider ... /> found.')
+  },
+  deactivate: () => {
+    invariant(false, 'No <Web3ReactProvider ... /> found.')
+  },
+  active: false
+})
+
+export function useWeb3React() {
+  return useContext(Web3ReactContext)
+}
